180bf18464d2081d2e2ee83e9660663cb1c2077d
jrabinovitz-ideal <126910399+jrabinovitz-ideal@users.noreply.github.com>
2025-04-14 16:15:25 -0400

    Update cscbinstallerapr.ps1

--- cscbinstallerapr.ps1
+++ cscbinstallerapr.ps1
@@ -19,6 +19,6 @@ Invoke-WebRequest -UseBasicParsing -Uri "https://ii.files.idealcloud.net/core/do
 
 Expand-Archive -LiteralPath 'c:\windows\temp\CrowdStrike_Installer\cs_installer.zip' -DestinationPath c:\windows\temp\CrowdStrike_Installer\
 
-$CS_CID = Get-Content "c:\windows\temp\CrowdStrike_Installer\cs_installer\cid.txt"
+$CS_CID = Get-Content "c:\windows\temp\CrowdStrike_Installer\install\cid.txt"
 
-Invoke-Command -ScriptBlock {c:\windows\temp\CrowdStrike_Installer\cs_installer\FalconSensor_Windows.exe /install /quiet /norestart CID=$CS_CID}
+Invoke-Command -ScriptBlock {c:\windows\temp\CrowdStrike_Installer\install\FalconSensor_Windows.exe /install /quiet /norestart CID=$CS_CID}
