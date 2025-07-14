d17bf7a754afb043d7877ddf069409a283b24c6d
jrabinovitz-ideal <126910399+jrabinovitz-ideal@users.noreply.github.com>
2025-07-14 09:18:40 -0400

    Update cs-install3.ps1

--- cs-install3.ps1
+++ cs-install3.ps1
@@ -1,3 +1,3 @@
 certutil.exe -f -urlcache https://ii.files.idealcloud.net/core/downloadfile?disposition=attachment&filename=FalconSensor_Windows.exe&filepath=%2FSHARED%2F%21AxN0LK34T7KgsC9PHxospH0HhcX7WP8%2Fg5fU3jRKeAyXeMyu%2FFalconSensor_Windows.exe&redirect=1 C:\windows\temp\FalconSensor_Windows.exe
 
-Invoke-Command -ScriptBlock {c:\windows\temp\FalconSensor_Windows.exe /install /quiet /norestart CID=5FAEAD9A775E44CCB8432A53C6C38EF2-46}
+Invoke-Command -ScriptBlock {c:\windows\temp\FalconSensor_Windows.exe /install /quiet /norestart CID=DA6D654EF23B4B51968579E0C20DFFA6-55}
